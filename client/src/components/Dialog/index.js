5a7b72279d1fc732598bb7fa5417e735d1691119
tuocha <kippon47@gmail.com>
2019-10-23 20:37:45 -0400

    added dialog box for account creation on landing page

---
+++ client/src/components/Dialog/index.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Checkbox from "../../components/Checkbox";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+      padding: theme.spacing(2)
+  },
+  content: {
+      marginBottom: 50
+  }
+}));
+
+export default function DialogBox() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = () => {
+    //logic to add info to the database
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Button variant="outlined" 
+      color="primary" 
+      onClick={handleClickOpen}
+      fullWidth
+      >
+        create an account
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Account creation</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="email address"
+            type="email"
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="username"
+            type="username"
+            fullWidth
+          />
+          <TextField
+            className={classes.content}
+            autoFocus
+            margin="dense"
+            id="name"
+            label="password"
+            type="password"
+            fullWidth
+          />
+          <DialogContentText>business or personal?</DialogContentText> 
+          <Checkbox />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            cancel
+          </Button>
+          <Button onClick={handleSubmit} color="primary">
+            submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
