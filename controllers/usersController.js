8f2045104d1c6f92fc690af1a156c1768f68e806
Danny Falciani <Daniel@danny-falcianis-macbook-pro.local>
2019-10-30 19:02:38 -0400

    fixed production SQL queries

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -28,7 +28,8 @@ module.exports = {
   findUserRatedDrinks2: function(req, res) {
     let query = "";
     if (process.env.NODE_ENV === "production") {
-      query = `Select dur.DrinkId, d.dislpay_name drink_Name, dur.UserId, u.display_name, dur.Rating, dur.Review, d.ingredient_Detail, d.instruction, dur.createdAt from c9o52bgidux04ro3.drink_user_ratings dur, c9o52bgidux04ro3.drinks d, c9o52bgidux04ro3.users u where 	dur.DrinkId = d.id and dur.UserId = u.id and u.id = ${req.params.id} order by dur.createdAt desc`;
+      query = `Select dur.DrinkId, d.dislpay_name drink_Name, dur.UserId, u.display_name, dur.Rating, dur.Review, d.ingredient_Detail, d.instruction, dur.createdAt from c9o52bgidux04ro3.Drink_User_Ratings dur, c9o52bgidux04ro3.Drinks d, c9o52bgidux04ro3.Users u where dur.DrinkId = d.id and dur.UserId = u.id and u.id = ${req.params.id} order by dur.createdAt desc;`
+      // query = `Select dur.DrinkId, d.dislpay_name drink_Name, dur.UserId, u.display_name, dur.Rating, dur.Review, d.ingredient_Detail, d.instruction, dur.createdAt from c9o52bgidux04ro3.drink_user_ratings dur, c9o52bgidux04ro3.drinks d, c9o52bgidux04ro3.users u where 	dur.DrinkId = d.id and dur.UserId = u.id and u.id = ${req.params.id} order by dur.createdAt desc`;
     } else {
       query = `Select dur.DrinkId, d.dislpay_name drink_Name, dur.UserId, u.display_name, dur.Rating, dur.Review, d.ingredient_Detail, d.instruction, dur.createdAt from neat_drinks_db.drink_user_ratings dur, neat_drinks_db.drinks d, neat_drinks_db.users u where 	dur.DrinkId = d.id and dur.UserId = u.id and u.id = ${req.params.id} order by dur.createdAt desc`;
     }
